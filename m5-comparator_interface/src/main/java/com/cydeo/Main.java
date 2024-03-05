0600ee10b5d3bba9c7ae3344b6d381720c216d8a
oraz <hudayberdiyevoraz@gmail.com>
2024-03-05 12:20:09 +0100

    Double Colon Operators

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,20,12,45,23);
+        System.out.println(list);
+
+        //Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Descending
+        Collections.sort(list,new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list,((o1, o2) -> (o1>o2)? -1: (o1<o2)? 1 : 0));
+        System.out.println(list);
+    }
+}
